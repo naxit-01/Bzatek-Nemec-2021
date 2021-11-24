6e47feb0264c55425a0fc94ad8b1da8fe0554e91
naxit-01 <bzatekv@gmail.com>
2021-11-24 15:31:07 +0100

    database handler

---
+++ BN_v.0.3/ROOT_server/moduls/__init__.py
@@ -0,0 +1,4 @@
+from .request_async import *
+from .admin import *
+from .tornroutes import *
+from .database import *
---
+++ BN_v.0.3/ROOT_server/moduls/database.py
@@ -0,0 +1,212 @@
+import sqlite3
+# Create a SQL connection to our SQLite database 
+"""
+def connect_database(database):  
+    return sqlite3.connect(database) 
+
+def close_connection(con):
+    con.close()
+
+def readTableNames(database):
+    con=connect_database(database)
+    res = con.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tablenames = []
+    for name in res.fetchall():
+        tablenames.append(name[0])
+    close_connection(con)
+    return tablenames
+
+def readTableRows(database, tablename):
+    con=connect_database(database)
+    cur=con.cursor()
+    query = "SELECT * FROM "+tablename
+    cur.execute(query)
+    tasks = cur.fetchall()
+    close_connection(con)
+    return tasks
+
+def readTableColumns(database, tablename):
+    con=connect_database(database)
+    cur=con.cursor()
+    res=cur.execute("SELECT * FROM "+tablename).description
+    columns =[]
+    for name in res:
+        columns.append(name[0])
+    close_connection(con)
+    return columns
+
+def addTableRow(database, tablename):
+    columns=readTableColumns(database, tablename)
+    command="INSERT INTO "+tablename+" VALUES ("
+    for key in enumerate(columns):
+        command+="'',"
+    command = command[:-1]
+    command+=")"
+
+    con=connect_database(database)
+    cur= con.cursor()
+    try: cur.execute(command)
+    except: print("kolize v id")
+    con.commit()
+    close_connection(con)
+
+def updateTableRow(database, tablename,data):
+    columns=readTableColumns(database, tablename)
+    command="UPDATE "+tablename+" SET "
+    for i,key in enumerate(columns):
+        command+=columns[i]+" = ? ,"
+    command = command[:-1]
+    command+=" WHERE id = ?"
+
+    con=connect_database(database)
+    cur = con.cursor()
+    try: cur.execute(command,data)
+    except: print("kolize v id")
+    con.commit()
+    close_connection(con)
+
+def deleteTableRow(database, tablename, id):
+    con=connect_database(database)
+    cur = con.cursor()
+    cur.execute("DELETE FROM "+ tablename +" WHERE id=?", (id,))
+    con.commit()
+    close_connection(con)
+
+def findPK(database, tablename):
+    con=connect_database(database)
+    cur = con.cursor()
+    command="PRAGMA table_info("+tablename+")"
+    cur.execute(command)
+    tasks = cur.fetchall()
+    close_connection(con)
+    for i,column in enumerate(tasks):
+        if column[5]==1:
+            return i
+
+    return 0
+
+def getNewID():
+    return str(5)
+
+def doesIdExist():
+    return True
+    
+def find_ID_in_database(database, tablename, arg,position):
+    con = connect_database(database)
+    list=readTableRows(database, tablename)
+    con = close_connection(con)
+
+    for i,record in enumerate(list):
+        if arg==list[i][position]:
+            return i
+
+    print("Error-findID didnt find anything arg: "+arg)
+    return "ERROR"
+"""
+
+
+class DBHandler():
+    _db_path = ""
+
+    def __init__(self,path):
+        self._db_path = path
+
+    def connect_database(self):  
+        return sqlite3.connect(self._db_path) 
+
+    def close_connection(self, con):
+        con.close()
+
+    def readTableNames(self):
+        con=self.connect_database()
+        res = con.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tablenames = []
+        for name in res.fetchall():
+            tablenames.append(name[0])
+        self.close_connection(con)
+        return tablenames
+
+    def readTableRows(self, tablename):
+        con=self.connect_database()
+        cur=con.cursor()
+        query = "SELECT * FROM "+tablename
+        cur.execute(query)
+        tasks = cur.fetchall()
+        self.close_connection(con)
+        return tasks
+
+    def readTableColumns(self, tablename):
+        con=self.connect_database()
+        cur=con.cursor()
+        res=cur.execute("SELECT * FROM "+tablename).description
+        columns =[]
+        for name in res:
+            columns.append(name[0])
+        self.close_connection(con)
+        return columns
+
+    def addTableRow(self, tablename):
+        columns=self.readTableColumns(tablename)
+        command="INSERT INTO "+tablename+" VALUES ("
+        for key in enumerate(columns):
+            command+="'',"
+        command = command[:-1]
+        command+=")"
+
+        con=self.connect_database()
+        cur= con.cursor()
+        try: cur.execute(command)
+        except: print("kolize v id")
+        con.commit()
+        self.close_connection(con)
+
+    def updateTableRow(self, tablename,data):
+        columns=self.readTableColumns(tablename)
+        command="UPDATE "+tablename+" SET "
+        for i,key in enumerate(columns):
+            command+=columns[i]+" = ? ,"
+        command = command[:-1]
+        command+=" WHERE id = ?"
+
+        con=self.connect_database()
+        cur = con.cursor()
+        try: cur.execute(command,data)
+        except: print("kolize v id")
+        con.commit()
+        self.close_connection(con)
+
+    def deleteTableRow(self, tablename, id):
+        con=self.connect_database()
+        cur = con.cursor()
+        cur.execute("DELETE FROM "+ tablename +" WHERE id=?", (id,))
+        con.commit()
+        self.close_connection(con)
+
+    def findPK(self, tablename):
+        con=self.connect_database()
+        cur = con.cursor()
+        command="PRAGMA table_info("+tablename+")"
+        cur.execute(command)
+        tasks = cur.fetchall()
+        self.close_connection(con)
+        for i,column in enumerate(tasks):
+            if column[5]==1:
+                return i
+
+        return 0
+
+    def getNewID():
+        return str(5)
+
+    def doesIdExist():
+        return True
+    
+    def find_ID_in_database(self, tablename, arg, position):
+        list=self.readTableRows(tablename)
+
+        for i,record in enumerate(list):
+            if arg==list[i][position]:
+                return i
+
+        print("Error-findID didnt find anything arg: "+arg)
+        return "ERROR"
