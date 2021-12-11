5f9b511f91263cb07600022dcbeaa1beb690ee85
josef-nemec-unob <75065544+josef-nemec-unob@users.noreply.github.com>
2021-12-11 20:19:07 +0100

    functional v.0.5

---
+++ BN_v.0.5/ui_server/UI_main.py
@@ -0,0 +1,66 @@
+import tornado.ioloop
+import tornado.web
+import json
+
+g_port=9998
+g_router="http://127.0.0.1:9999"
+
+def createHtmlPage(firstPart, getUrl, secondPart):
+	f = open("template/tmp.html", "w")
+
+	with open(firstPart, "r") as infile: #copy infile into f
+		f.write(infile.read())
+	f.write(getUrl)
+	with open(secondPart, "r") as infile:
+		f.write(infile.read())
+
+	f.close()
+
+def createHtmlPageParams(firstPart, getUrl, secondPart, params, thirdPart):
+	f = open("template/tmp.html", "w")
+
+	with open(firstPart, "r") as infile: #copy infile into f
+		f.write(infile.read())
+	f.write(getUrl)
+	with open(secondPart, "r") as infile:
+		f.write(infile.read())
+	f.write(str(params))
+	with open(thirdPart, "r") as infile:
+		f.write(infile.read())
+
+	f.close()
+
+class mainPage(tornado.web.RequestHandler):
+	def get(self, uri):
+		'''Render page with javascript get requests to api (through router)'''
+		if self.cookies:
+			print("I received some cookies from the scout girls.")
+			print(self.get_secure_cookie("UserType"), self.get_secure_cookie("UserID"))
+
+		parsed = uri.split("/")
+		url = str('"' + g_router + "/api/" + uri)
+
+		if parsed[0] == "rozvrh":
+			data = (("params", json.dumps({
+				'datum':"1.1.2022",
+				'type':"prednasky"
+			})))
+
+			createHtmlPageParams("template/page1of2or3.html", url, "template/page2of3.html", data, "template/page3of3.html")
+			f = open("template/tmp.html") # Opened to get the current (latest) version, with simple render he used the file created after first run on every attempt
+			self.write(f.read())
+		else:
+			createHtmlPage("template/page1of2or3.html", url, "template/page2of2.html")
+			f = open("template/tmp.html") # Opened to get the current (latest) version, with simple render he used the file created after first run on every attempt
+			self.write(f.read())
+
+		f.close()
+
+application = tornado.web.Application([
+    (r"/(.*)", mainPage)
+], cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__")
+
+if __name__ == "__main__":
+	application.listen(g_port)
+	print("UI_tornado running on port: " + str(g_port) + "...")
+	tornado.ioloop.IOLoop.instance().start()
